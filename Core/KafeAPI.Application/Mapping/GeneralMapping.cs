0887d250fe3cb36c6a7cd4018ce535b2dee5db91
beyzaaydogan01 <aydoganoglubeyza@gmail.com>
2026-02-13 11:28:44 +0300

    mapping added

---
+++ Core/KafeAPI.Application/Mapping/GeneralMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using KafeAPI.Application.Dtos.CategoryDtos;
+using KafeAPI.Application.Dtos.MenuItemDtos;
+using KafeAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeAPI.Application.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, DetailCategoryDto>().ReverseMap();
+
+            CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
+            CreateMap<MenuItem, UpdateMenuItemDto>().ReverseMap();
+            CreateMap<MenuItem, ResultMenuItemDto>().ReverseMap();
+            CreateMap<MenuItem, DetailMenuItemDto>().ReverseMap();
+        }
+    }
+}
