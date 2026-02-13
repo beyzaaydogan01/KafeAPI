21e90a07c832147f08dab2d55c5a02df7c7f5f8a
beyzaaydogan01 <aydoganoglubeyza@gmail.com>
2026-02-13 11:26:57 +0300

    generic repo added

--- Core/KafeAPI.Application/Interfaces/IGenericRepository.cs
+++ Core/KafeAPI.Application/Interfaces/IGenericRepository.cs
@@ -12,6 +12,6 @@ namespace KafeAPI.Application.Interfaces
         Task<List<T>> GetAllAsync();
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        Task DeleteAsync(T entity);
     }
 }
